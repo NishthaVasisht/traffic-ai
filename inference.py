b37aa0e82cc685a71ad5df762f401c44679b4f36
Nishtha Vasisht <nishthapctebtech19cse@gmail.com>
2026-04-06 15:13:21 +0530

    switch to python openenv mode

---
+++ inference.py
@@ -0,0 +1,46 @@
+import random
+
+class TrafficEnv:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.state = [
+            random.randint(0, 10),
+            random.randint(0, 10),
+            random.randint(0, 10),
+            random.randint(0, 10),
+            0
+        ]
+        return self.state
+
+    def step(self, action):
+        # simple logic
+        reward = -sum(self.state[:4])
+        self.state = [
+            max(0, s - random.randint(0, 2)) for s in self.state[:4]
+        ] + [action]
+
+        return self.state, reward, False, {}
+
+env = TrafficEnv()
+
+def reset():
+    obs = env.reset()
+    return {
+        "observation": obs,
+        "reward": 0,
+        "terminated": False,
+        "truncated": False,
+        "info": {}
+    }
+
+def step(action):
+    obs, reward, done, info = env.step(action)
+    return {
+        "observation": obs,
+        "reward": reward,
+        "terminated": False,
+        "truncated": False,
+        "info": {}
+    }
